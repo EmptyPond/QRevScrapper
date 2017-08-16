209f660198c00fc82bf9af3b975a05a4fb27594f
Kevin Kawai <kevin.fredricks@gmail.com>
2017-08-16 08:43:49 +0000

    Added Bootstrap,simple_form and rspec gems

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module QrevenueScrapper
   class Application < Rails::Application
+    #For loading Lib and Vendor
+    config.eager_load_paths += [ Rails.root.join('lib'), Rails.root.join('vendor') ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ lib/scrapper.rb
@@ -0,0 +1,19 @@
+#Note that when using gemfiles the require command must have the same name as specified in the gemfile ie. all lowercase for this project
+require 'nokogiri'
+require 'httparty'
+require 'json'
+require 'pry'
+require 'csv'
+
+class Scrapper
+  def scrape
+    page = HTTParty.get('https://www.sec.gov/Archives/edgar/data/4962/0000004962-04-000203.txt')
+    parse_page = Nokogiri::HTML(page)
+
+    return parse_page.css('table')[1].to_s.match(/Total\s*[0-9\,]*/)[0].gsub(/\s+/,' ')
+  end
+end
+
+value = Scrapper.new.scrape
+
+#Pry.start(binding)
