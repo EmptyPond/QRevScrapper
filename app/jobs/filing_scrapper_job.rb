2243f43c7dd5447735a8ce2e964b0df6f69a873a
Kevin Kawai <kevin.fredricks@gmail.com>
2017-08-24 07:15:52 +0000

    Updated scrapper to the actual new one

--- app/jobs/filing_scrapper_job.rb
+++ app/jobs/filing_scrapper_job.rb
@@ -2,7 +2,7 @@ class FilingScrapperJob < ApplicationJob
   queue_as :default
 
   def perform(filing)
-    val = Scrapper.new.scrape
+    val = Scrapper.new(year=filing[:year].to_s,quarter=filing[:quarter].to_s,company=filing[:company]).scrape
     val = val.split(' ')[1].delete(',').to_i
     filing.update_attributes(total_rev:val)
     ActionCable.server.broadcast "filing_channel", update: filing
